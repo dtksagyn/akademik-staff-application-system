dae7f9535ba6bdba24cfea405c4d6e754c44d1cd
dtksagyn <dtksagyn@gmail.com>
2025-03-13 16:06:21 +0300

    WIP: My changes in progress

--- models/Scientific_Professional_Activities.js
+++
@@ -1,8 +0,0 @@
-const activitySchema = new mongoose.Schema({
-    applicant: { type: mongoose.Schema.Types.ObjectId, ref: 'Applicant', required: true },
-    membership: String,
-    editorialRole: String,
-    conferences: String
-});
-
-module.exports = mongoose.model('Activity', activitySchema);
