ea346bfdbb52647f65f864754cf6b276fe26b3dd
dtksagyn <dtksagyn@gmail.com>
2025-03-14 13:46:27 +0300

    hh

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,9 +4,6 @@ const router = express.Router();
 
 router.post("/signup", userController.signUp); // Correct route path
 
-// Signup route
-router.post("/signup", userController.signUp);
-
 // Login route
 router.post("/login", userController.login);
 
