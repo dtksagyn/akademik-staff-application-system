665e173a9ca1a51640b2a28bdfaf10d7ce51d4b7
dtksagyn <161740245+dtksagyn@users.noreply.github.com>
2026-04-01 17:29:49 +0300

    Delete frontend/application/backend directory

--- frontend/application/backend/models/Documents.js
+++
@@ -1,8 +0,0 @@
-const documentSchema = new mongoose.Schema({
-    applicant: { type: mongoose.Schema.Types.ObjectId, ref: 'Applicant', required: true },
-    documentType: { type: String, enum: ['National ID', 'Passport', 'Proof of Employment', 'Academic Certificates'] },
-    fileUrl: String,
-    uploadedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Document', documentSchema);
--- frontend/application/backend/models/awards.js
+++
@@ -1,9 +0,0 @@
-const awardSchema = new mongoose.Schema({
-    applicant: { type: mongoose.Schema.Types.ObjectId, ref: 'Applicant', required: true },
-    awardName: String,
-    awardingInstitution: String,
-    year: Number,
-    certificateUrl: String // File URL
-});
-
-module.exports = mongoose.model('Award', awardSchema);
